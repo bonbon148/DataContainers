9a81e177e920ea0a8e21a3464e1c6b8364e061ba
bonbon148 <townpony608@gmail.com>
2026-03-25 12:21:05 +0300

    Fibonacci 1

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 double Power(double a, int n);
+void Fibonacci(int n, int a = 0, int b = 1);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
 	int a;
-	cout << "¬ведите основание степени: "; cin >> a;
-	cout << "¬ведите показатель степени: "; cin >> n;
-	cout << Power(a, n) << endl;
+	//cout << "¬ведите основание степени: "; cin >> a;
+	//cout << "¬ведите показатель степени: "; cin >> n;
+	//cout << Power(a, n) << endl;
+	cout << "¬ведите предел р€да ‘ибоначчи: "; cin >> n;
+	Fibonacci(n, 22, 33);
+	cout << endl;
 
 	main();
 }
@@ -23,4 +27,11 @@ double Power(double a, int n)
 	//else if (n > 0)return a * Power(a, n - 1);
 	//else return 1 / Power(a, -n);
 	//else return 1 / a * Power(a, n + 1);
+}
+
+void Fibonacci(int n, int a, int b)
+{
+	if (a > n)return;
+	cout << a << "\t";
+	Fibonacci(n, b, a + b);
 }
